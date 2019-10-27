276ce1f7b13c40812f5388d04fc8a7290ffdf310
Mannex Pimentel <mannex247@gmail.com>
2019-10-26 23:29:51 -0400

    create and register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const User = require('../../models/User.js');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @route POST api/users
 
@@ -13,12 +16,38 @@ router.post(
     check("email", "Email Is Requied").isEmail(),
     check("password", "Please enter Password").isLength({ min: 5 })
   ],
-  (req, res) => {
+  async(req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+    try{
+        let user = await User.findOne({ email });
+
+        if(user){
+           return res.status(400).json({ errors: [{ msg: "User already exits" }] });
+        }
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        })
+        user = new User({
+            name,
+            email,
+            password
+        });
+        const salt = await bcrypt.hash(password, salt);
+
+        await user.save();
+        res.send("User Registered");
+    } 
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
   }
 );
 
