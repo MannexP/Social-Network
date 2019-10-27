5e2689ac9236977d2e71f336a0f112a280527570
Mannex Pimentel <mannex247@gmail.com>
2019-10-26 21:40:22 -0400

    register user logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route GET api/users
+// @route POST api/users
 
-router.get('/', (req,res) => res.send ('User Route'));
+router.post(
+  "/",
+  [
+    check("name", "Name Is Required")
+      .not()
+      .isEmpty(),
+    check("email", "Email Is Requied").isEmail(),
+    check("password", "Please enter Password").isLength({ min: 5 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
