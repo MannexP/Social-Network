39575871559ead1dee564c153279af8e6358214e
Mannex Pimentel <mannex247@gmail.com>
2019-11-23 19:48:44 -0500

    postman check

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,29 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator/check");
 
 // @route GET api/profile/me
 // @desc  GET current users profile
-// @access GET Privat
+// @access GET Private
 
-
-router.get('/me', auth, async (req,res) => {
-try {
+router.get("/me", auth, async (req, res) => {
+  try {
     const Profile = await Profile.findOne({
-        user: req.user.id}).populate(
-            'user'['name','avatar']
-        );
-    if(!profile){
-        return res.status(400).json({
-            msg:'There is no profile for this user'});
+      user: req.user.id
+    }).populate("user"[("name", "avatar")]);
+    if (!profile) {
+      return res.status(400).json({
+        msg: "There is no profile for this user"
+      });
     }
     res.json(profile);
-} catch {
+  } catch {
     console.error(err.message);
-    res.status(500).send('Server Error from Profile.js')
-}
+    res.status(500).send("Server Error from Profile.js");
+  }
 });
+
+// @route POST api/profile
+// @desc  Create or Update user profile
+// @access  Private
+
+router.post("/", [
+  auth,
+  [
+    check("status", "Status is required")
+      .not()
+      .isEmpty(),
+    check("skills", "Skills is required").not().isEmpty()
+  ], async (req,res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({errors:errors.array()});
+      }
+  }
+]);
 
 module.exports = router;
