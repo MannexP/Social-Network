7a6ef2dbe43956c311466acd09c29d954f0603e6
Mannex Pimentel <mannex247@gmail.com>
2020-01-18 20:25:59 -0500

    Post model created

---
+++ models/Posts.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+// not necessary but cleaner than constantly typing mongoose.schema
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  // this connects user to posts
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        // by tying likes to user and user and userId user cannot double like
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        // by tying likes to user and user and userId user cannot double like
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoos.model('post', PostSchema);
