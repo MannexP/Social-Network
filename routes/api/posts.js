15fbfe2e4bebb85ee397490a181b9829e7775061
Mannex Pimentel <mannex247@gmail.com>
2020-01-19 21:30:46 -0500

    add like

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -34,17 +34,17 @@ router.post(
       const user = await User.findById(req.user.id).select("-password");
 
       //   set new post object. Text comes from body, the rest comes from the user model(name, avatar), and user is for just the ID
-    //   new Post instantiates a new post from the model
-      const newPost = new Post( {
+      //   new Post instantiates a new post from the model
+      const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id
       });
 
-    //  above is the new post made. Save new post in post and send back in response json.
+      //  above is the new post made. Save new post in post and send back in response json.
       const post = await newPost.save();
-      res.json(post)
+      res.json(post);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
@@ -56,69 +56,100 @@ router.post(
 // @desc GET all Post
 // @access Private
 
-router.get('/', auth, async (req, res) => {
-    try {
-        // 
-        const posts = await Post.find().sort({ date: -1});
-        res.json(posts)
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error')
-    }
+router.get("/", auth, async (req, res) => {
+  try {
+    //
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route GET api/posts/:id
 // @desc GET post by ID
 // @access Private
 
-router.get('/:id', auth, async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if(!post){
-            return res.status(404).json({msg: 'Post not found'});
-        }
-
-        res.json(post)
-    } catch (err) {
-        console.error(err.message);
-        // error has a kind method 
-        if(err.kind === 'ObjectId'){
-            return res.status(404).json({msg: 'Post not found'});
-        }
-        res.status(500).send('Server Error')
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
     }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    // error has a kind method
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route Delete api/posts/:id
 // @desc Delete post by ID
 // @access Private
 
-router.delete('/:id', auth, async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
 
-        if(!post){
-            return res.status(404).json({msg: 'Post not found'});
-        }
-
-
-        // check to see if the user owns post in order to delete
-        // req.user.id is a string, and post.user is an object, so we make post.user into a string.
-        if(post.user.toString() !== req.user.id){
-            // 401 is not authorizes
-            return res.status(401).json({msg: 'User Not Authorized'});
-        }
-
-        await post.remove();
-
-        res.json({msg: 'Post removed'})
-    } catch (err) {
-        console.error(err.message);
-        if(err.kind === 'ObjectId'){
-            return res.status(404).json({msg: 'Post not found'});
-        }
-        res.status(500).send('Server Error')
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
     }
-})
+
+    // check to see if the user owns post in order to delete
+    // req.user.id is a string, and post.user is an object, so we make post.user into a string.
+    if (post.user.toString() !== req.user.id) {
+      // 401 is not authorizes
+      return res.status(401).json({ msg: "User Not Authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route PUT api/posts/like/:id
+// @desc Like a post
+// @access Private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked by this user
+    if (
+        // if current user equals req.user.id and if that length is greater than 0, it has already been liked. if so return a 400 req
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0)
+     {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    // if not greater than 0 than take that posts like and add to the begining of the array with unshift and add the user with req.user
+    post.likes.unshift({ user: req.user.id });
+
+    // save to data base
+    await post.save();
+
+    // return posts 
+    res.json(post.likes);
+     
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
