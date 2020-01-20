a71f7c6a5a462360b186899f483157a668eb3043
Mannex Pimentel <mannex247@gmail.com>
2020-01-19 22:00:08 -0500

    delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -126,14 +126,13 @@ router.delete("/:id", auth, async (req, res) => {
 
 router.put("/like/:id", auth, async (req, res) => {
   try {
-
     const post = await Post.findById(req.params.id);
 
     // Check if the post has already been liked by this user
     if (
-        // if current user equals req.user.id and if that length is greater than 0, it has already been liked. if so return a 400 req
-      post.likes.filter(like => like.user.toString() === req.user.id).length > 0)
-     {
+      // if current user equals req.user.id and if that length is greater than 0, it has already been liked. if so return a 400 req
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
       return res.status(400).json({ msg: "Post already liked" });
     }
 
@@ -143,9 +142,8 @@ router.put("/like/:id", auth, async (req, res) => {
     // save to data base
     await post.save();
 
-    // return posts 
+    // return posts
     res.json(post.likes);
-     
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -156,86 +154,129 @@ router.put("/like/:id", auth, async (req, res) => {
 // @access Private
 
 router.put("/unlike/:id", auth, async (req, res) => {
-    try {
-  
-      const post = await Post.findById(req.params.id);
-  
-      // Check if the post has already been liked by this user
-      if (
-          // if current user equals req.user.id and if that length is greater than 0, it has already been liked. if so return a 400 req
-        post.likes.filter(like => like.user.toString() === req.user.id).length === 0)
-       {
-        return res.status(400).json({ msg: "Post has not been liked" });
-      }
-  
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked by this user
+    if (
+      // if current user equals req.user.id and if that length is greater than 0, it has already been liked. if so return a 400 req
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post has not been liked" });
+    }
+
     //  Get remove index, this gets the like
-    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
 
     // this splices it out of the array
-      post.likes.splice(removeIndex, 1);
-  
-      // save to data base
+    post.likes.splice(removeIndex, 1);
+
+    // save to data base
+    await post.save();
+
+    // return posts
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST api/posts/comment/:id
+// @desc  Comment on a post
+// @access Private
+
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "Text is Required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //   since we are using error checking we set it first
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //                                       errors has array method, this will return the errors
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //                       logged in protected route, giving access to token and ID in req.user. using select to not send back password in json
+      const user = await User.findById(req.user.id).select("-password");
+
+      const post = await Post.findById(req.params.id);
+
+      //   set new post object. Text comes from body, the rest comes from the user model(name, avatar), and user is for just the ID
+      //   new Post instantiates a new post from the model
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
       await post.save();
-  
-      // return posts 
-      res.json(post.likes);
-       
+      res.json(post.comments);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
-  });
+  }
+);
 
-  // @route POST api/posts/comment/:id
-  // @desc  Comment on a post
-  // @access Private
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc  Delete a comment
+// @access Private
 
-router.post(
-    "/comment/:id",
-    [
-      auth,
-      [
-        check("text", "Text is Required")
-          .not()
-          .isEmpty()
-      ]
-    ],
-    async (req, res) => {
-      //   since we are using error checking we set it first
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        //                                       errors has array method, this will return the errors
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      try {
-        //                       logged in protected route, giving access to token and ID in req.user. using select to not send back password in json
-        const user = await User.findById(req.user.id).select("-password");
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    // get post by id
+    const post = await Post.findById(req.params.id);
 
-        const post = await Post.findById(req.params.id);
-  
-        //   set new post object. Text comes from body, the rest comes from the user model(name, avatar), and user is for just the ID
-        //   new Post instantiates a new post from the model
-        const newComment = {
-          text: req.body.text,
-          name: user.name,
-          avatar: user.avatar,
-          user: req.user.id
-        };
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
 
-        post.comments.unshift(newComment)
-  
-        await post.save();
-        res.json(post.comments);
-      } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-      }
+    // make sure comment exits
+
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
     }
-  );
 
-  // @route POST api/posts/comment/:id
-  // @desc  Comment on a post
-  // @access Private
+    // Check if user deleting comment is the user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+     //  Get remove index, this gets the like
+     const removeIndex = post.comments
+     .map(comment => comment.user.toString())
+     .indexOf(req.user.id);
+
+   // this splices it out of the array
+   post.comments.splice(removeIndex, 1);
+
+   // save to data base
+   await post.save();
+
+   // return comments
+   res.json(post.comments);
+
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
